e90876d86ceaec24fbff9ab2c2420bf917bcf322
Germain <germain.leignel@gmail.com>
2023-02-24 14:45:40 +0100

    tt

--- cypress/e2e/linkedin-test/second_scenario.cy.js
+++ cypress/e2e/linkedin-test/second_scenario.cy.js
@@ -1,1 +1,16 @@
 /// <reference types="cypress" />
+describe('Simulation d\'un nouveau post sur linkedin', () => {
+
+    it('Se connecter à linkedin', () => {
+        
+            cy.clearAllCookies();
+            cy.clearLocalStorage();
+            cy.visit('https://www.linkedin.com/');
+        
+        
+            cy.get('#session_key').type('gejevod102@wwgoc.com');
+            cy.get('#session_password').type('Password');
+            cy.get('.sign-in-form__submit-button').click({force: true});
+    });
+        
+});
