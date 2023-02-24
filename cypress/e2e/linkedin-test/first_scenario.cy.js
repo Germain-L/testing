2d9111fb2082c76d6db1a885151a6d2ee74960c8
thomasBarille-ab <thomasbarille@gmail.com>
2023-02-24 15:59:54 +0100

    login test

--- cypress/e2e/linkedin-test/first_scenario.cy.js
+++ cypress/e2e/linkedin-test/first_scenario.cy.js
@@ -3,21 +3,54 @@
 describe('Simulation d\'un parcour utilisateur sur linkedin', () => {
 
     it('Navigation sur le lien linkedin', () => {
+        cy.on('uncaught:exception', (err, runnable) => {
+                            return false;
+                        })
         cy.clearAllCookies();
         cy.clearLocalStorage();
         cy.visit('https://www.linkedin.com/');
+
+    });
+
+    it('Vérification du bouton mot de passe oublié', () => {
+        cy.on('uncaught:exception', (err, runnable) => {
+                            return false;
+                        })
+        cy.contains('Forgot password?').click({force: true});
+    });
+
+    it('Vérification du champs Email/Phone et des boutons Reset Password et Back', () => {
+        cy.on('uncaught:exception', (err, runnable) => {
+                                    return false;
+                                })
+        cy.get('#username').should('exist');
+        cy.get('#reset-password-submit-button').should('exist');
+        cy.get('#back-button-password-reset').should('exist');
+    });
+
+    it('Test click bouton Back', () => {
+        cy.on('uncaught:exception', (err, runnable) => {
+                                    return false;
+                                })
+        cy.get('#back-button-password-reset').click({force: true});
     });
 
     it('Vérification de la présence du champ mail et mot de passe, puis du bouton connexion', () => {
-        cy.get('#session_key').should('exist');
-        cy.get('#session_password').should('exist');
-        cy.get('.sign-in-form__submit-button').should('exist');
+        cy.on('uncaught:exception', (err, runnable) => {
+                                    return false;
+                                })
+        cy.get('input[type=text]').should('exist');
+        cy.get('input[type=password]').should('exist');
+        cy.contains('Sign in').should('exist');
     });
 
     it('Connexion de l\'utilisateur', () => {
-        cy.get('#session_key').type('gejevod102@wwgoc.com');
-        cy.get('#session_password').type('Password');
-        cy.get('.sign-in-form__submit-button').click({force: true});
+        cy.on('uncaught:exception', (err, runnable) => {
+                                    return false;
+                                })
+        cy.get('input[type=text]').type('gejevod102@wwgoc.com');
+        cy.get('input[type=password]').type('Password');
+        cy.contains('Sign in').click({force: true});
     });
 
 });
